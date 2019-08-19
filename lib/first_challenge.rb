4bd4acc0b9a4bc669fe02652d47105e813988b59
Takeshi Kimoto <lnuk2009jp@gmail.com>
2019-08-19 13:56:11 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,6 +1,6 @@
 def first_challenge
    epic_tragedy = {
-  "first_last1" => {name: , age: },
-  "first_last2" => {name: , age: }
+  :first_last1 => {name: , age: },
+  :first_last2 => {name: , age: }
 }
 end
